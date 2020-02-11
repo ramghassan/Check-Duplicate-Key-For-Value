cfaa332b4a8a82c823a397c099045df0b6aa5cc2
Ramasubramanian Rajaram <ramasubramanian260@gmail.com>
2020-02-11 14:45:39 +0530

    Update that support both JS and JSON file

--- checkDuplicateEntry.js
+++ checkDuplicateEntry.js
@@ -1,4 +1,5 @@
 import readline from 'readline';
+import fs from 'fs';
 
 let stringMsg = 'There is no duplicate value present'; 
 const readFromTerminal = readline.createInterface({
@@ -6,14 +7,51 @@ const readFromTerminal = readline.createInterface({
     output: process.stdout
   })
 
-readFromTerminal.question(`Enter the File Absolute Path : `, async (filePath) => {
-    console.log(`\nEntired file path is ---- : ${filePath}!`)
-    readFromTerminal.question(`\nEnter Main Key of object in which u need to find that has duplicate : `, async (key) => {
-        console.log(`\n Enter Main Key ---:  ${key}!`)
-				var fileData = await require(filePath);
-				checkDuplicateData(fileData.default[key])
+	readFromTerminal.question(`Select the type data \n
+Select 1 for data is a Object in exported in JS file \n
+Select 2 for data which is JSON Object in JSON file \n
+Select 1 or 2 : `, async (selectedNo) => {
+	if (selectedNo === '1') getDataForObj();
+	else if (selectedNo === '2') getJSONData();
+	else console.log('Please enter correct value')
+});
+
+const getDataForObj = () => {
+	readFromTerminal.question(`\n
+Enter the File Absolute Path : `, async (filePath) => {
+		console.log(`\nEntired file path is ---- : ${filePath}!`);
+		readFromTerminal.question(`\nEnter Main Key of object in which u need to find that has duplicate : `, async (key) => {
+			console.log(`\n Enter Main Key ---:  ${key}!`)
+			var fileData = await require(filePath);
+			if (fileData) {
+				const data = (fileData.default && fileData.default[key]) || fileData[key]
+				if (data) {
+					checkDuplicateData(data)
+				} else {
+					console.log('Please enter the correct Key');
+				}
+			} else {
+				console.log('Please enter the correct file path')
+			}
 		});
-  });
+	});
+}
+
+const getJSONData = () => {
+	readFromTerminal.question(`Enter the JSON File Absolute Path : `, async (filePath) => {
+		console.log(`\nEntired file path is ---- : ${filePath}`)
+		let parsedData = '';
+		fs.readFile(filePath, (err, data) => {
+			try {
+				parsedData = JSON.parse(data);
+				checkDuplicateData(parsedData);
+			} catch(err) {
+				console.log('Please enter the valid path or check whether it is a proper JSON file')
+			}
+		});
+	});
+}
+
 
   const checkDuplicateData = (data) => {
 		  
@@ -38,6 +76,6 @@ readFromTerminal.question(`Enter the File Absolute Path : `, async (filePath) =>
 				}
 			});
 			
-			console.log(`\nThe Results ============== \n ${msg || stringMsg}`)
+			console.log(`\nThe Results ============== \n\n ${msg || stringMsg}`)
 			console.log(`\nTOTAL NUMBER OF DUPLICATE KEY FOR THE VALUE ${serialNo}`)
   }
